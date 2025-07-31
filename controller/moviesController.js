54f57fc2eb050965f02d25dea4119a568166df97
DavideCriscuolo <davide.alcatel@gmail.com>
2025-07-31 16:03:33 +0200

    creata rotta show pe mostrare il singolo film

--- controller/moviesController.js
+++ controller/moviesController.js
@@ -5,12 +5,27 @@ const index = (re, res) => {
   connection.query(sql, (err, results) => {
     if (err) {
       console.log(err);
+      return res.status(500).json({ err: err.message });
+    }
+    console.log(results);
+    res.json(results);
+  });
+};
+
+const show = (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM `movies` WHERE `id` = ?;";
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ err: err.message });
     } else {
-      res.send(results);
+      res.json(results[0]);
     }
   });
 };
 
 module.exports = {
   index,
+  show,
 };
