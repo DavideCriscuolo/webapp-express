204875ee5e6fa0921cd19008afbe05eb283f3bc3
DavideCriscuolo <davide.alcatel@gmail.com>
2025-07-31 16:41:17 +0200

    inserite middleware e assets statici

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 const moviesRoUter = require("./routes/movies");
+app.use(express.static("public"));
 app.use(express.json());
 app.use("/movies", moviesRoUter);
+
+//middleware per  gestione degli errori
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ message: "Errore nel server" });
+});
+//middleware per  gestione delle rotte non trovate
+app.use((req, res, next) => {
+  res.status(404).send({ message: "Rotta non trovata" });
+});
+
 app.listen(port, () => {
   console.log(`Il server è collegato alla porta  http://localhost:${port}`);
 });
